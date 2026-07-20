4cba261cec25fe99ac0aafa65a05f14a108696a3
Rafay Raza <rafayraza2091@gmail.com>
2026-07-20 16:51:29 +0500

    refactor: migrate FontAwesome stylesheet import from layout head to globals CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* FontAwesome CDN per spec */}
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
-        />
       </head>
       <body className={inter.className}>
         {children}
